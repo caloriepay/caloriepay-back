487dc13abfd76973b77c9021a318114edb264d16
jaehunLee-git <joz526@naver.com>
2024-09-05 20:30:11 +0900

    first commit

---
+++ src/main/java/com/pknu/caloriepay/CaloriepayApplication.java
@@ -0,0 +1,13 @@
+package com.pknu.caloriepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pknu/caloriepay/CaloriepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pknu.caloriepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
